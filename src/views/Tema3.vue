cff462095ba6bdcdb19783a43f669b92c1c4f8c1
Sergio Camacho <sergio.camachoo@misena.edu.co>
2021-08-25 15:56:46 -0500

    Avances 5 Tema3, Tema4

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -3,17 +3,35 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    Muestras
+    .titulo-principal(data-aos="flip-up")
+      .titulo-principal__numero
+        span 3
+      h1 Diagrama de cadena de suministro
+
+    p.mb-5 En la cadena de suministro existen actividades de proceso productivo que una entidad debe contemplar. A continuación, ver figura 1, se evidencia un diagrama de las actividades en orden desde las principales hasta las de apoyo:
+
+    .row.justify-content-center
+      .col-10
+        .titulo-sexto.color-acento-contenido
+          span Figura 1. Diagrama de cadena de suministro.
+        figure.mb-5
+          img(src="@/assets/curso/tema3-imagen1.svg")
+          figcaption Nota. White (2014).
+
+        p.mb-5 Así aparecen entidades especializadas en el flujo de la cadena de suministro, que dan respuesta a las necesidades de mejorar la producción, reducción de costos y midiendo los tiempos de suministro; aquí coordinan coherentemente las diferentes actividades dando un enfoque a las materias primas, fábrica, entrega y todas las actividades de apoyo.
+
+        .row.align-items-end.row-intro-with-box
+          .col-4
+            figure
+              img(src="@/assets/curso/tema3-imagen2.svg")
+          .col-8
+            p.mb-3 En la cadena de suministro intervienen factores clave para la coordinación, aprovisionamiento, producción y distribución, que garantizan un proceso productivo certificado. Posteriormente a través un sistema de control en la logística segura y de transporte, se distribuya eficientemente los productos y lograr así la satisfacción del cliente.
   
 </template>
 
 <script>
-import Muestras from '../components/Muestras'
 export default {
   name: 'Tema3',
-  components: {
-    Muestras,
-  },
   data: () => ({
     // variables de vue
   }),
--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -3,17 +3,74 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    Muestras
+    .titulo-principal(data-aos="flip-up")
+      .titulo-principal__numero
+        span 4
+      h1 Sistemas de información y comunicación
+
+    .row.justify-content-center
+      .col-lg-10
+        .cajon.color-primario.p-5.mb-5
+          p.mb-0 El modo en que se realiza la comunicación en una empresa es fundamental para lograr un proceso efectivo en el suministro de un bien o servicio; puesto que, si no existen unos canales de comunicación ya definidos, abrirá paso para errores que afecten la satisfacción del cliente. 
+
+        p.mb-5 Para lograr una buena planeación en la cadena de suministro es importante conocer las características del producto, esto se define con un proceso logístico que revisó todos los diferentes escenarios para la cadena productiva y allí se controla la calidad, almacenaje y manipulación del producto o servicio, por lo que se tiene en cuenta al fabricante con mejores cualidades o características, pues ofrecen descuentos, acompañamiento técnico, valor unitario y al por mayor, para así determinar qué compra será productiva y cuál no.
+
+        figure.mb-5
+          img(src="@/assets/curso/tema4-imagen1.svg")
+
+        p.mb-5 Dentro de la información es necesario conocer los tipos de compras productivas en las que recaen <strong>las materias primas, componentes, embalajes, etiquetas e inclusive la subcontratación de servicios.</strong>
+          br
+          br
+          | Dentro de las no productivas están:
+
+        .row
+          .col-lg-7.mb-lg-0.mb-5
+            ol.lista-ol--cuadro
+              li 
+                .lista-ol--cuadro__vineta
+                  span 1
+                | Las compras de maquinaria.
+              li 
+                .lista-ol--cuadro__vineta
+                  span 2
+                | Programas y equipos informativos.
+              li 
+                .lista-ol--cuadro__vineta
+                  span 3
+                | Transporte.
+              li 
+                .lista-ol--cuadro__vineta
+                  span 4
+                | Suministros de agua y luz.
+              li 
+                .lista-ol--cuadro__vineta
+                  span 5
+                | Arriendos de locales y oficinas.
+              li 
+                .lista-ol--cuadro__vineta
+                  span 6
+                | Servicios de mantenimiento.
+              li 
+                .lista-ol--cuadro__vineta
+                  span 7
+                | Avisos publicitarios.
+              li 
+                .lista-ol--cuadro__vineta
+                  span 8
+                | Repuestos y accesorios.
+              li 
+                .lista-ol--cuadro__vineta
+                  span 9
+                | Interventorías y consultorías que a la final le dan alto valor a la empresa.
+          .col-lg-5
+            figure
+              img(src="@/assets/curso/tema4-imagen2.svg")
   
 </template>
 
 <script>
-import Muestras from '../components/Muestras'
 export default {
   name: 'Tema4',
-  components: {
-    Muestras,
-  },
   data: () => ({
     // variables de vue
   }),
