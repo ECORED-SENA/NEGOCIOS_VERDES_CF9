f40c4c10ea2a9f34fb2bc1bb08de1409294ae719
Sergio Camacho <sergio.camachoo@misena.edu.co>
2021-10-12 09:31:49 -0500

    Agregan los videos

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -12,7 +12,7 @@
 
     figure.mb-5(data-aos="fade-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/YTiEwBGKrvA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     .row.justify-content-center(data-aos="fade-right" data-aos-duration="1200")
       .col-lg-8.col-12
--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -61,7 +61,7 @@
 
     figure.mb-5(data-aos="fade-in" data-aos-duration="1000")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/VBKPoAhAeS8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     Separador(data-aos="fade-in" data-aos-duration="1000")
 
